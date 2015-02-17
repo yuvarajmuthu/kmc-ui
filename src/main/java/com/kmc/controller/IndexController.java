23864215dfcfaceaad599235402f0c068f1a14b4
yuvarajmuthu <yuvaraj.muthu@gmail.com>
2015-02-16 23:03:41 -0500

    made root as index1.html

--- src/main/java/com/kmc/controller/IndexController.java
+++ src/main/java/com/kmc/controller/IndexController.java
@@ -9,19 +9,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
-/**
- * Created with IntelliJ IDEA.
- * User: xvitcoder
- * Date: 12/20/12
- * Time: 5:27 PM
- */
+
 @Controller
 @RequestMapping("/")
 public class IndexController {
 
     @RequestMapping
     public String getIndexPage() {
-        return "index";
+        return "index1";
     }
     
     @RequestMapping("login")
